0970f98c28a31341e3249071f711ec83fdb8a248
dogyeom <sprfv@naver.com>
2025-10-20 02:49:24 +0900

    Spring core 모듈 의존성 관리 학습 중 (chap01)

---
+++ src/main/java/com/codeit/springcore/chap01/KimChef.java
@@ -0,0 +1,9 @@
+package com.codeit.springcore.chap01;
+
+public class KimChef {
+    // 의존하고 있는 객체가 따로 없기 때문에 쿡만 써도 괜츈
+    public void cook() {
+        System.out.println("안녕하세요. Kim Chef입니다.");
+        System.out.println("요리를 시작합니다.");
+    }
+}
