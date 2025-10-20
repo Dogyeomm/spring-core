84bae4d3376133d16c8d55ccd142e78f8f0d9694
dogyeom <sprfv@naver.com>
2025-10-20 17:29:58 +0900

    Controller 클래스 생성 및 빈 등록, 간단한 요청 처리해 보기

--- src/main/java/com/codeit/springcore/HomeController.java
+++ src/main/java/com/codeit/springcore/HomeController.java
@@ -2,8 +2,9 @@ package com.codeit.springcore;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // 컨트롤러 역할 빈을 등록하겠다. Component랑 똑같은데, 이름으로 역할 구분하려고.
 public class HomeController {
 
     public HomeController() {
@@ -11,6 +12,7 @@ public class HomeController {
     }
 
     @GetMapping("/hello")
+    @ResponseBody // home.html 말고 home이라는 문자 갖다 박으려고 붙인 거임
     public String home() {
         System.out.println("서버에 요청이 들어왔음!");
         return "home";
